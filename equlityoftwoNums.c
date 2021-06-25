028c1c880d4ba197d64e428e1e80068155fc7091
Sailesh Pandey <saileshpandey007007@gmail.com>
2021-06-25 23:13:42 +0530

    Two numbers are equal or not

---
+++ equlityoftwoNums.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+    
+    printf("Enter the first number");
+    scanf("%d",&a);
+    printf("Enter the second number");
+    scanf("%d",&b);
+    
+    if(a==b)
+    printf("The numbers are equal");
+    else
+    printf("The number are not equal");
+    
+    
+    
+    
+    
+    
+    return 0;
+}
